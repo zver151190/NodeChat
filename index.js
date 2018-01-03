a4e7baa0f3c9e0631ae4580a4aad4311c596d3bd
zver151190 <15kain@gmail.com>
2018-01-03 14:58:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .get('/', (req, res) => res.sendFile('pages/index.html'))
-  .get('/dashboard', (req, res) => res.sendFile('pages/dashboard.html'))
-  .get('/chat', (req, res) => res.sendFile('pages/chat.html'))
+  .get('/', (req, res) => res.render(__dirname+'public/views/pages/index.html'))
+  .get('/dashboard', (req, res) => res.render(__dirname+'public/views/pages/dashboard.html))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
