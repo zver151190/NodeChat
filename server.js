942a740dce6cabf58744b825dbf8939b29738428
zver151190 <15kain@gmail.com>
2018-01-03 13:23:53 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express')
+const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
-const path = require('path')
-const io = require('socket.io')(http)
-const PORT = process.env.PORT || 5000
+const path = require('path');
+const io = require('socket.io')(http);
+const PORT = process.env.PORT || 5000;
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
