513d17b1a984de823a0df8e0ffbfd152842c5ff7
zver151190 <15kain@gmail.com>
2018-01-03 19:54:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,10 +10,12 @@ app.use(express.static(path.join(__dirname, 'public')))
 server.listen(process.env.PORT || 5000);
 
 
-mongodb.connect(uri, function(err, client) {        
-io.on('connection', function(socket){ 
 
-  
+mongodb.connect(uri, function(err, client) {
+var global_socket;  
+io.on('connection', function(socket){ 
+  global_socket = socket;
+});  
           app.get('/', function(req, res){
             res.sendFile(__dirname + '/views/pages/index.html');
               var key = req.query.k;
@@ -21,19 +23,19 @@ io.on('connection', function(socket){
               var email = req.query.email;
               var user_id = req.query.user_id;
               var result = {username:username,email:email,user_id:user_id};
-              socket.emit('userInfo',result); 
+              global_socket.emit('userInfo',result); 
           });
 
 
 
-                socket.on('startUserChat', function (userId) { 
+                global_socket.on('startUserChat', function (userId) { 
                     const db = client.db('nodejs');
                     db.collection("chat").find({user_id:userId}).toArray(function(err, result) {
-                      var global_socket;.emit('renderChat',result); 
+                      global_socket.emit('renderChat',result); 
                     });
                  });
 
-                socket.on('sendMessage', function (data) {
+                global_socket.on('sendMessage', function (data) {
                   const db = client.db('nodejs');
                   var user_id = data.user_id;
                   var email = data.email;
@@ -43,6 +45,5 @@ io.on('connection', function(socket){
                   var timestamp = d.getTime();
                   db.collection("chat").update( {user_id:user_id},{$push:{messages:{ user_id: user_id,creation_time:timestamp, username: username,email:email,message:message,type:"user" }}} );
                 });
-    });
 
 });
