baec6db3903c23bbc812b4b1d89f5477133919e9
zver151190 <15kain@gmail.com>
2018-01-03 15:30:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express'),
+var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server),
