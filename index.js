4674938d4565b2270f1cf8907d782f728f78752c
zver151190 <15kain@gmail.com>
2018-01-03 15:13:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const mongodb = require('mongodb').MongoClient
 const path = require('path')
+const io = require('socket.io')(http)
+var url = ' mongodb://admin:054398262@ds239217.mlab.com:39217/nodejs'
 const PORT = process.env.PORT || 5000
 
 express()
@@ -8,3 +10,24 @@ express()
   .get('/', (req, res) => res.sendFile(__dirname + '/views/pages/index.html'))
   .get('/dashboard', (req, res) => res.sendFile(__dirname + '/views/pages/dashboard.html'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+mongodb.connect(url, function(err, client) {
+    io.on('connection', function(socket){
+      socket.on('startUserChat', function (userId) {
+        const db = client.db('nodejs');
+        db.collection("chat").find({user_id:userId}).toArray(function(err, result) {
+          socket.emit('renderChat',result); 
+        });
+      });
+      socket.on('sendMessage', function (data) {
+        const db = client.db('mongochat');
+        var user_id = data.user_id;
+        var email = data.email;
+        var username = data.username;
+        var message = data.message;
+        var d = new Date();
+        var timestamp = d.getTime();
+        db.collection("chat").update( {user_id:user_id},{$push:{messages:{ user_id: user_id,creation_time:timestamp, username: username,email:email,message:message,type:"user" }}} );
+      });
+    });
+});
