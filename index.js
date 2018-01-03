5edee60d58a1713e86ccf8695904c4b80cc6491d
zver151190 <15kain@gmail.com>
2018-01-03 14:59:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .get('/', (req, res) => res.render(__dirname+'public/views/pages/index.html'))
-  .get('/dashboard', (req, res) => res.render(__dirname+'public/views/pages/dashboard.html))
+  .get('/', (req, res) => res.sendFile(__dirname + 'public/views/pages/index.html'))
+  .get('/dashboard', (req, res) => res.sendFile(__dirname + 'public/views/pages/dashboard.html))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
