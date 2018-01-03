536f2d71806199934446d35052d4b63d7e233149
zver151190 <15kain@gmail.com>
2018-01-03 14:46:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const PORT = process.env.PORT || 5000
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/dashboard', (req, res) => res.render('pages/dashboard'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
