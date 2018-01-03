8a6483b5b1d09f4aafd369b942f347dfb69aed76
zver151190 <15kain@gmail.com>
2018-01-03 13:31:49 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express');
+const app = express();
 const MongoClient = require('mongodb').MongoClient;
 const path = require('path');
 const io = require('socket.io')(http);
 const PORT = process.env.PORT || 5000;
 const url = 'mongodb://admin:0543982262@ds239217.mlab.com:39217/nodejs';
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'ejs');
+  app.get('/', (req, res) => res.render('pages/index'));
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 
 MongoClient.connect(url, function(err, client) {
