a2ff5b159db752024872c07b9daf9e322f2735f1
zver151190 <15kain@gmail.com>
2018-01-09 21:53:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
-
-app.get('/', function(req, res){
-         res.sendFile(__dirname + '/views/pages/index.html');       
-});
-
+var express = require('express')
+var app = express()
+var server = require('http').createServer(app)
+var io = require('socket.io').listen(server)
+const mongodb = require('mongodb').MongoClient
+const path = require('path')
+var uri = 'mongodb://'+process.env.MONGO_USER+':'+process.env.MONGO_PASS+'@ds239217.mlab.com:39217/nodejs'
 var clients = {};
 
-io.on('connection', function (socket) {
-  clients[socket.id] = socket;
+
+
+app.use(express.static(path.join(__dirname, 'public')))
   
-  console.log('a user connected: ' + socket.id);
-  socket.emit("test", "ping");
-  
-  socket.on('test', function (e) {
-    console.log(e);
-  });
-  
-  socket.on("disconnect",function(data){
-    delete clients[socket.id];
-  });
+app.get('/', function(req, res){
+         res.sendFile(__dirname + '/views/pages/index.html');
+           key = req.query.k;
+           username = req.query.username;
+           email = req.query.email;
+           isClient = true;
+           user_id = req.query.user_id;
+           result = {username:username,email:email,user_id:user_id};         
 });
+
+  io.on('connection', function(socket){
+     socket.on('room', function(room) {
+              socket.join(room);
+              clients[socket.id] = socket;
+              console.log("connect---"+socket.id);
+      });
+          
+      socket.on('disconnect', function() {
+          console.log("disconnect---"+socket.id);
+          delete clients[socket.id];
+      });
+  });
+
+
+
+server.listen(process.env.PORT || 5000);
+
+
+
+
+
