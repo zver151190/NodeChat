1043a2b02383a6447b5f4aaa9602ed549fcf4b97
zver151190 <15kain@gmail.com>
2018-01-09 21:40:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,97 +1,18 @@
-var express = require('express')
-var app = express()
-var server = require('http').createServer(app)
-var io = require('socket.io').listen(server)
-const mongodb = require('mongodb').MongoClient
-const path = require('path')
-var uri = 'mongodb://'+process.env.MONGO_USER+':'+process.env.MONGO_PASS+'@ds239217.mlab.com:39217/nodejs'
+var io = require('socket.io').listen(server);
+
 var clients = {};
-var client_arr = [];
-var key;
-var username;
-var email;
-var user_id;
-var result;
-var isClient = false;
 
-
-app.use(express.static(path.join(__dirname, 'public')))
+io.sockets.on('connection', function (socket) {
+  clients[socket.id] = socket;
   
-app.get('/', function(req, res){
-         res.sendFile(__dirname + '/views/pages/index.html');
-           key = req.query.k;
-           username = req.query.username;
-           email = req.query.email;
-           isClient = true;
-           user_id = req.query.user_id;
-           result = {username:username,email:email,user_id:user_id};         
+  console.log('a user connected: ' + socket.id);
+  socket.emit("test", "ping");
+  
+  socket.on('test', function (e) {
+    console.log(e);
+  });
+  
+  socket.on("disconnect",function(data){
+    delete clients[socket.id];
+  });
 });
-
-app.get('/dashboard', function(req, res){
-         res.sendFile(__dirname + '/views/pages/dashboard.html');       
-});
-
-
-mongodb.connect(uri, function(err, client) {
-        io.on('connection', function(socket){
-          
-           socket.on('room', function(room) {
-              socket.join(room);
-           });
-          if(isClient){
-            clients[socket.id] = socket;
-            var online_client = {client_id:socket.id,username:result.username,email:result.email,user_id:result.user_id};
-            var clientExists = false;
-            for( i = 0; i < client_arr.length ; i++ ){
-              console.log("CHECK "+client_arr[i].user_id+'-----'+result.user_id);
-               if(client_arr[i].user_id == result.user_id ){
-                 clientExists = true;
-               }
-            }
-            if(!clientExists){
-              client_arr.push(online_client);
-            }
-            socket.to('dashboard').emit('onlineClient',online_client);
-          }
-          
-            socket.emit('userInfo',result);
-
-            socket.on('startUserChat', function (userId) { 
-                 const db = client.db('nodejs');
-                 db.collection("chat").find({user_id:userId}).toArray(function(err, result) {
-                        socket.emit('renderChat',result); 
-                  });
-             });
-          
-             socket.on('checkOnlineClients', function (data) {
-                  socket.emit('checkOnlineClients',client_arr); 
-             });
-          
-             socket.on('sendMessage', function (data) {
-                    const db = client.db('nodejs');
-                    var user_id = data.user_id;
-                    var email = data.email;
-                    var username = data.username;
-                    var message = data.message;
-                    var d = new Date();
-                    var timestamp = d.getTime();
-                    var update_obj = { user_id: user_id,creation_time:timestamp, username: username,email:email,message:message,type:"user" };
-                    db.collection("chat").update( {user_id:user_id},{$push:{messages:{ user_id: user_id,creation_time:timestamp, username: username,email:email,message:message,type:"user" }}} );
-                    socket.emit('sendMessageResponse',update_obj);
-                    socket.to('dashboard').emit('clientSentMessage', update_obj);
-              });
-          
-             socket.on('disconnect', function() {
-                socket.to('dashboard').emit('offlineClient',online_client);
-                delete clients[socket.id];
-                for( i = 0; i < client_arr.length ; i++ ){
-                   console.log(client_arr[i].username);
-                }
-             });
-       });
-}); 
-server.listen(process.env.PORT || 5000);
-
-
-
-
